4c22ac580109d74b96a6103be83e435ee1770acd
BARAZAJ <barazajohn52@gmail.com>
2024-11-22 17:43:24 +0300

    new

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true },
-    name: { type: String, required: true },
-    ipaddress: { type: String, required: true },
+    firstname: { type: String, required: true, unique: true },
+    lastname: { type: String, required: true },
+    favoritecolor: { type: String, required: true },
+    birthdate: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);

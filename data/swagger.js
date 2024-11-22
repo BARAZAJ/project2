7e652dd2e20f5e262bbe6b84b25e3ee6266c8e38
BARAZAJ <barazajohn52@gmail.com>
2024-11-22 19:23:17 +0300

    ndes

--- data/swagger.js
+++ data/swagger.js
@@ -16,7 +16,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: 'http://localhost:3001', // Replace with your production URL when deploying
+                url: 'https://project1-370z.onrender.com/', // Replace with your production URL when deploying
             },
         ],
         components: {
